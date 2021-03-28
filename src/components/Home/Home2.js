49f7bb1e2b786f42fe1ac959655fad45596892ba
maneeshmogallapu <63709683+maneeshmogallapu@users.noreply.github.com>
2021-03-29 01:17:44 +0530

    Add files via upload

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/image2.png";
+import "../../style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Tilt from "react-parallax-tilt";
+
+function Home2() {
+  return (
+    <Container fluid className=" home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            I am a Student at VIT-AP pursuing M.Tech( software engineering ). I am intersted in web development ,I’m currently learning react js.🤷‍♂️
+              <br />
+              <br />I can work on
+              <i>
+                <b className="purple"> java, Python and Javascript. </b>
+              </i>
+              <br />
+              <br />
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/maneeshmogallapu"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/manesshkumarM"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/maneeshmogallapu/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/maneeshmogallapu/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
